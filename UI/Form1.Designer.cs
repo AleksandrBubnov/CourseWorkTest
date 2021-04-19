e90fe8e3dc316e68722552068430b39a794b4142
AleksandrBubnov <alexnfs@mail.ru>
2021-04-19 11:21:27 +0300

    Create UI

--- UI/Form1.Designer.cs
+++ UI/Form1.Designer.cs
@@ -29,13 +29,100 @@ namespace UI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonNewTest = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.statusStrip1.Location = new System.Drawing.Point(0, 234);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(496, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.buttonEdit);
+            this.groupBox1.Controls.Add(this.buttonNewTest);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Location = new System.Drawing.Point(16, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(464, 210);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // buttonNewTest
+            // 
+            this.buttonNewTest.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonNewTest.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.buttonNewTest.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonNewTest.Location = new System.Drawing.Point(32, 78);
+            this.buttonNewTest.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonNewTest.Name = "buttonNewTest";
+            this.buttonNewTest.Size = new System.Drawing.Size(148, 82);
+            this.buttonNewTest.TabIndex = 0;
+            this.buttonNewTest.Text = "New Test";
+            this.buttonNewTest.UseVisualStyleBackColor = false;
+            this.buttonNewTest.Click += new System.EventHandler(this.buttonNewTest_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonEdit.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.buttonEdit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonEdit.Location = new System.Drawing.Point(285, 78);
+            this.buttonEdit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(148, 82);
+            this.buttonEdit.TabIndex = 1;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = false;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(496, 256);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximumSize = new System.Drawing.Size(550, 330);
+            this.MinimumSize = new System.Drawing.Size(512, 295);
+            this.Name = "Form1";
+            this.Opacity = 0.98D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonNewTest;
     }
 }
 
---
+++ XmlClassLibrary/Answer.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml.Serialization;
+using System.Collections.Generic;
+
+
+namespace XmlClassLibrary
+{
+    [XmlRoot(ElementName = "Answer")]
+    public class Answer
+    {
+        [XmlElement(ElementName = "Description")]
+        public string Description { get; set; }
+        [XmlElement(ElementName = "IsRight")]
+        public string IsRight { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Question")]
+    public class Question
+    {
+        [XmlElement(ElementName = "Description")]
+        public string Description { get; set; }
+        [XmlElement(ElementName = "Difficulty")]
+        public string Difficulty { get; set; }
+        [XmlElement(ElementName = "Answer")]
+        public List<Answer> Answer { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Test")]
+    public class Test
+    {
+        [XmlElement(ElementName = "Author")]
+        public string Author { get; set; }
+        [XmlElement(ElementName = "TestName")]
+        public string TestName { get; set; }
+        [XmlElement(ElementName = "Qty_questions")]
+        public string Qty_questions { get; set; }
+        [XmlElement(ElementName = "Question")]
+        public List<Question> Question { get; set; }
+        [XmlAttribute(AttributeName = "xsi", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Xsi { get; set; }
+        [XmlAttribute(AttributeName = "xsd", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Xsd { get; set; }
+    }
+}
